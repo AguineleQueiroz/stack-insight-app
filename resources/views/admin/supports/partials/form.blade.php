c9c5e11ca4ae06d531f0df6e1a198fe98f7fb5d5
AguineleQueiroz <aguinele.queiroz@gafit.com.br>
2023-08-11 12:14:41 -0300

    ref: views organization

--- resources/views/admin/supports/partials/form.blade.php
+++ resources/views/admin/supports/partials/form.blade.php
@@ -1,4 +1,10 @@
 @csrf
-<input type="text" name="subject" placeholder="Doubt's title" value="{{ $support->subject ?? old('subject')}}">
-<textarea name="content_body" cols="30" rows="5" placeholder="Doubt's description"> {{ $support->content_body ?? old('content_body')}}</textarea>
+<label for="subject">
+    Subject
+    <input type="text" name="subject" placeholder="Doubt's title" value="{{ $support->subject ?? old('subject')}}">
+</label>
+<label for="content_body">
+    Content Support
+    <textarea name="content_body" cols="30" rows="5" placeholder="Doubt's description"> {{ $support->content_body ?? old('content_body')}}</textarea>
+</label>
 <button type="submit">Submit</button>
