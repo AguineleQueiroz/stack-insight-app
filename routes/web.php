804161c5d6417058a92b255aed8077443103e03c
AguineleQueiroz <aguinele.queiroz@gafit.com.br>
2023-12-26 20:05:37 -0300

    Feat: full autentication added

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Services\Enums\SupportStatus;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Site\{SiteController};
 use App\Http\Controllers\Admin\{SupportController};
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -14,15 +18,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    /* application */
+    Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
+    Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
+    Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
+    Route::get('/supports/{id}',[SupportController::class, 'show'])->name('supports.show');
+    Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
+    Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
+    Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
+    Route::get('/findSupport/{id}', [SupportController::class, 'findSupport']);
 });
 
 require __DIR__.'/auth.php';
-
-Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
-Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
-Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
-Route::get('/supports/{id}',[SupportController::class, 'show'])->name('supports.show');
-Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
-Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
-Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
-Route::get('/findSupport/{id}', [SupportController::class, 'findSupport']);
